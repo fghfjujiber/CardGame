f838d610877fda0d9da08aef0fe67c53600f3e86
Xiaoming Chen <xiaomingchen@Xiaomings-MacBook-Pro.local>
2015-08-19 12:16:34 +0800

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by Xiaoming Chen on 15/8/19.
+//  Copyright (c) 2015年 Jackchenxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CardGame
+//
+//  Created by Xiaoming Chen on 15/8/19.
+//  Copyright (c) 2015年 Jackchenxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
